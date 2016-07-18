434d07f2dcb685cfe0aa41bf04e46a1fcb7cd7fc
Sam Jenkins <samjenkins@Sams-MacBook-Air.local>
2016-07-18 21:12:30 +1200

    push all files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# powersout
-# powersout
+# Powersout
+
+This is a tech demo of a progressive web app that uses service workers, it's a proof of concept using the idea of an outages app that you might want available on your phone.
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,6 +5,7 @@ module.exports = {
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
     '/src/app-shell.html',
     '/images/**.*'
+    '/images/{touch,appicon,favicon,regions}**.*'
   ],
   navigateFallback: '/index.html'
 };
