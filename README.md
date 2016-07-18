cedff80eb9299a39f5c6be73f16f78ae1a04156b
Sam Jenkins <samjenkins@Sams-MacBook-Air.local>
2016-07-18 20:48:03 +1200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # powersout
+# powersout
---
+++ sw-precache-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/src/app-shell.html',
+    '/images/**.*'
+  ],
+  navigateFallback: '/index.html'
+};
